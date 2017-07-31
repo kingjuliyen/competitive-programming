80b8d9871224af775caf3ea8b9a3da7d895b9abe
SenthilKumar.Thangavelu <kingjuliyen@gmail.com>
2017-07-31 16:37:12 +0530

    sk3

--- UVA/Graph/11906/sol-11906.cpp
+++ UVA/Graph/11906/sol-11906.cpp
@@ -4,6 +4,8 @@
 // author: Senthil Kumar Thangavelu, email: kingjuliyen @ google's email.com
 
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 struct Cell {
@@ -17,11 +19,21 @@ Cell Cell::BADCELL () { return Cell(-5, -5); }
 class KWG { // Knight War Grid
 public:
   int R, C, M, N, x, y;
+  vector<bool> spl; // search path list
+  vector<Cell> wtrCl; // water cells
+
   KWG(int _R, int _C, int _M, int _N) : R(_R), 
-  C(_C), M(_M), N(_N), x(0), y(0) { }
+  C(_C), M(_M), N(_N), x(0), y(0) { 
+    spl = vector<bool>(R*C, false);
+  }
+
+  #define SPLVAL spl[(c.x * C + c.y)]
+  bool isFoundInSearchPathListAlready(Cell &c) { return SPLVAL == true; }
+  void addToSearchPathList(Cell &c) { SPLVAL = true; }
+  void removeFromSearchPathList(Cell &c) { SPLVAL = false; }
 
   bool outOfBounds(Cell &c) {
-    return (c.x >= 0 && c.x < C && c.y >=0 && c.y < R) ? true : false;
+    return (c.x >= 0 && c.x < C && c.y >=0 && c.y < R) ? false : true;
   }
 
   bool isMoveValid(Cell & s, Cell &d) {
@@ -29,19 +41,18 @@ public:
   }
 
   bool isWaterBlocked(Cell &s, Cell &d) {
+    int x0 = min(s.x, d.x), x1 = max(s.x, d.x);
+    int y0 = min(s.y, d.y), y1 = max(s.y, d.y);
+
+    for(auto i = wtrCl.begin(), e = wtrCl.end(); i != e; ++i) {
+      int wx = i->x, wy = i->y;
+      if( (wy == s.y && wx >= x0 && wx <= x1) ||
+          (wx == d.x && wy >= y0 && wy <= y1) )
+        return true;
+    }
     return false;
   }
 
-  bool isFoundInSearchPathListAlready(Cell &src) {
-    return false;
-  }
-
-  void addToSearchPathList(Cell &c) {
-  }
-
-  void removeFromSearchPathList(Cell &c) {
-  }
-  
   void updateReachability(Cell &d, Cell &s) {
   }
 
@@ -54,32 +65,26 @@ public:
     } \
   } while(0)
 
-  void dfs(Cell cur) {
-    if(isFoundInSearchPathListAlready(cur))
+  void dfs(Cell src) {
+    if(isFoundInSearchPathListAlready(src))
       return;
 
-    addToSearchPathList(cur);
+    addToSearchPathList(src);
     {
-      tryNextMove(cur, cur.x + M, cur.y - N); // MHR_NVU,
-      tryNextMove(cur, cur.x - M, cur.y - N); // MHL_NVU,
-      tryNextMove(cur, cur.x + M, cur.y + N); // MHR_NVD,
-      tryNextMove(cur, cur.x - M, cur.y + N); // MHL_NVD, 
+      tryNextMove(src, src.x + M, src.y - N); // MHR_NVU,
+      tryNextMove(src, src.x - M, src.y - N); // MHL_NVU,
+      tryNextMove(src, src.x + M, src.y + N); // MHR_NVD,
+      tryNextMove(src, src.x - M, src.y + N); // MHL_NVD, 
 
-      tryNextMove(cur, cur.x + N, cur.y - M); // NHR_MVU,
-      tryNextMove(cur, cur.x - N, cur.y - M); // NHL_MVU,
-      tryNextMove(cur, cur.x + N, cur.y + M); // NHR_MVD,
-      tryNextMove(cur, cur.x - N, cur.y + M); // NHL_MVD,
+      tryNextMove(src, src.x + N, src.y - M); // NHR_MVU,
+      tryNextMove(src, src.x - N, src.y - M); // NHL_MVU,
+      tryNextMove(src, src.x + N, src.y + M); // NHR_MVD,
+      tryNextMove(src, src.x - N, src.y + M); // NHL_MVD,
     }
-    removeFromSearchPathList(cur);
+    removeFromSearchPathList(src);
   }
 
-  void markWaterSquare(int _x, int _y) {
-
-  }
-
-  void solve() {
-    dfs(Cell(0,0));
-  }
+  void markWaterSquare(int _x, int _y) { wtrCl.push_back(Cell(_x, _y)); }
 };
 
 int main() {
@@ -96,6 +101,6 @@ int main() {
       cout << " x " << x << " y " << y << "\n";
       kwg.markWaterSquare(x, y);
     }
-    kwg.solve();
+    kwg.dfs(Cell(0,0));
   }
 }
