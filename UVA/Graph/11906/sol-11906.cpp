265ec389abe85a272801e91f4a322e8747da2721
camel <camel@camels-MacBook-Pro.local>
2017-08-03 15:20:42 +0530

    cleanup after ac

--- UVA/Graph/11906/sol-11906.cpp
+++ UVA/Graph/11906/sol-11906.cpp
@@ -3,60 +3,43 @@
 // https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=229&page=show_problem&problem=3057
 // author: Senthil Kumar Thangavelu, email: kingjuliyen @ google's email.com
 
-#include <stdio.h>
 #include <iostream>
 #include <vector>
 #include <map>
 using namespace std;
 
+typedef pair<int, int> point;
+typedef pair <point, point> dst_src_pair;
+
 class KWG
 { // Knight War Grid
 public:
-  int R, C, M, N, odds, evens;
+  int R, C, M, N;
   vector<int> deltas;
-  map< pair<int, int>, bool > vst;
-  map< pair<int, int>, bool > waterMap;
+  map< pair<int, int>, bool > vst, waterMap;
+  map<dst_src_pair, bool> uniqPath;
+  map <point, int> pthCount;
 
-  KWG(int _R, int _C, int _M, int _N) : R(_R),
-    C(_C), M(_M), N(_N), odds(0), evens(0) 
+  KWG(int _R, int _C, int _M, int _N) : R(_R), C(_C), M(_M), N(_N)
   {
-      if(M != N)
-        deltas = {  M, -N,    M, N,   -M, N,    -M, -N,
-                  N, -M,    N, M,   -N, M,    -N, -M };
-      else
-        deltas = {  M, -N,    M, N,   -M, N,    -M, -N };
+    if(M != N)
+      deltas = { M,-N, M,N, -M,N, -M,-N, /* NM -> */ N,-M,  N,M, -N,M, -N,-M };
+    else
+      deltas = {  M, -N,    M, N,   -M, N,    -M, -N };
   }
 
-  void markWaterSquare(int x, int y) { 
-    waterMap[make_pair(x,y)] = true;
-  }
-
+  bool validSquare(int x, int y) { return (x>=0 && x<R && y>=0 && y<C); }
+  void markWaterSquare(int x, int y) { waterMap[make_pair(x,y)] = true; }
   bool waterSquare(int x, int y) {
     auto it = waterMap.find(make_pair(x,y));
     return (it == waterMap.end()) ? false : it->second;
   }
 
-  bool validSquare(int x, int y) { 
-    //cout << " C " << C << " R " << R << " x " << x << " y " << y << endl;
-    return (x>=0 && x<R && y>=0 && y<C);
-    // return (x>=0 && x<C && y>=0 && y<R); 
-  }
-  int getAsOffset(int x, int y) { return (x * C + y); }
-  int getAsOffset2(int x, int y) { return (y * C + x); }
-
-  void markVisited(int x, int y) {
-    vst[make_pair(x,y)] = true;
-  }
-
+  void markVisited(int x, int y) { vst[make_pair(x,y)] = true; }
   bool visited(int x, int y) {
     auto it = vst.find(make_pair(x,y));
     return (it == vst.end()) ? false : it->second;
   }
-
-  typedef pair<int, int> point;
-  typedef pair <point, point> dst_src_pair;
-  map<dst_src_pair, bool> uniqPath;
-  map <point, int> pthCount;
 
   void incrPathCount(point d, point s) {
     dst_src_pair dsp = make_pair(d,s);
@@ -64,12 +47,7 @@ public:
     if(it == uniqPath.end()) {
       uniqPath[dsp] = true;
       auto it2 = pthCount.find(d);
-      if(it2 == pthCount.end()) {
-        pthCount[d] = 1;
-      }
-      else {
-        pthCount[d] = pthCount[d] + 1;
-      }
+      pthCount[d] = (it2 == pthCount.end())? 1 : pthCount[d] + 1;
     }
   }
 
@@ -89,12 +67,8 @@ public:
     for(int i=0; i<deltas.size()/2; i++) {
       int x1 = x + deltas[i*2];
       int y1 = y + deltas[(i*2)+1];
-      // if(x == 16 && y == 10)
-      //   printf("src %d,%d    dst %d,%d\n", x, y, x1, y1);
-
       if(!okForNextJump(x1, y1))
         continue;
-      // printf("**src=%d,%d    **dst=%d,%d\n", x, y, x1, y1);
       incrPathCount(make_pair(x1, y1),  make_pair(x, y));
       dfs(x1, y1);
     }
@@ -109,9 +83,8 @@ public:
       else
         evens += 1;
     }
-    // cout << " evens " << evens << " odds " << odds << endl;
     if(!odds && !evens)
-    evens = 1;
+      evens = 1;
     cout << "Case " << caseId << ":" << " " << evens << " " << odds << "\n";
   }
 
@@ -121,20 +94,16 @@ public:
   }
 }; // class KWG
 
-int main()
-{
+int main() {
   int T = -1, caseId = 0;
   cin >> T;
   while (T--)
   {
     int R = -1, C = -1, M = -1, N = -1, W = -1, x = -1, y = -1;
     cin >> R >> C >> M >> N;
-    // cout << "caseid " << (caseId+1) << endl;
-    // cout << R << " " << C << " " << M << " " << N << endl;
     cin >> W;
     KWG kwg(R, C, M, N);
-    while (W--)
-    {
+    while (W--) {
       cin >> x >> y;
       kwg.markWaterSquare(x, y);
     }
