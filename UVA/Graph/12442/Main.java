0c36333c4ada26d88327fd36d9dcb8cd053de1c5
SenthilKumar.Thangavelu <kingjuliyen@gmail.com>
2017-08-09 21:25:02 +0530

    Only cpp solution works

--- UVA/Graph/12442/Main.java
+++ UVA/Graph/12442/Main.java
@@ -7,148 +7,91 @@ import java.lang.*;
 import java.math.*;
 
 class Main {
-
   class FE { // Forwarding Emails
-    ArrayList< ArrayList<Integer> > paths;
+    ArrayList<Integer> paths;
+    ArrayList <Boolean> visited;
+    ArrayList<Integer> mrc; // max reachability cache
+    int rcount, mdepth;
     int NUM_NODES() { return 50000 + 8; }
+    int maxR; // max Reachable vertexes during dfs
+    int V;    // vertex from which max vertexes can be reached
 
     FE () {
-      paths = new ArrayList < ArrayList<Integer> > (NUM_NODES());
-      for(int i=0 ; i<NUM_NODES() - 3 ; i++) { paths.add(null); }
+      paths = new ArrayList<Integer> (NUM_NODES());
+      visited = new ArrayList<Boolean> (NUM_NODES());
+      mrc = new ArrayList<Integer> (NUM_NODES());
+      for(int i=0 ; i<NUM_NODES() - 3; i++) { paths.add(-1); }
+      for(int i=0 ; i<NUM_NODES() - 3 ; i++) { visited.add(false); }
+      for(int i=0 ; i<NUM_NODES() - 3 ; i++) { mrc.add(-1); }
     }
 
-    void addPath(int u, int v) {
-      ArrayList<Integer> edges = paths.get(u); 
-      if(edges == null) {
-        edges = new ArrayList<Integer> (); 
-        paths.set(u, edges); 
-      }
-      edges.add(v);
+    void clearAll() {
+      for(int i=0 ; i<NUM_NODES() - 3; i++) { paths.set(i, -1); }
+      for(int i=0 ; i<NUM_NODES() - 3 ; i++) { visited.set(i, false); }
+      for(int i=0 ; i<NUM_NODES() - 3 ; i++) { mrc.set(i, -1); }
+      V = maxR = -1;
+      rcount = mdepth = 0;
     }
 
-    int rcount; // reachability count
-    boolean visitMap[];
-    void resetVisitMap() { visitMap = new boolean[NUM_NODES()]; }
-    boolean visited(int v) { return visitMap[v]; }
-    void setVisited(int v) { visitMap[v] = true; }
+    int getConnection(int v) { return paths.get(v); }
+    void addPath(int u, int v) { paths.set(u, v); }
+    int getMaxReachable(int v) { return mrc.get(v); }
+    void setMaxReachable(int v, int r) { mrc.set(v, r); }
+    void clearAllVisited() { for(int i=0 ; i<NUM_NODES()-3; i++) { visited.set(i, false); } }
+    void markAsVisited(int v) { visited.set(v, true); }
+    boolean visited(int v) { return visited.get(v); }
+    void initForDfs() { clearAllVisited(); rcount = mdepth = 0;}
 
     void dfs(int v) {
-      if(visited(v))
-        return;
-      setVisited(v);
-      rcount++; 
-
-      ArrayList<Integer> edges = paths.get(v);
-      if(edges == null) {
-        rcount = 0;
+      // System.out.println("dfs v "+v);
+      if(visited(v)) {
+        mdepth = rcount;
         return;
       }
-      for(int i=0; i<edges.size(); i++) {
-        int v1 = edges.get(i);
-        dfs(v1);
-      }
+      markAsVisited(v);
+
+      rcount++;
+      int nv = getConnection(v);
+      if(nv != -1)
+        dfs(nv);
+      setMaxReachable(v, mdepth - rcount);
+      rcount--;
     }
 
-    int chdR[];
-    void initCachedR() {
-      chdR = new int[NUM_NODES()]; 
-      for(int i=0; i<NUM_NODES(); i++) { chdR[i] = -1; }
-    }
-    void setCachedR(int v, int r) { chdR[v] = r; }
-    int cachedR(int v) { return chdR[v]; }
-
-    // reScore - reachability scores for each vertex
-    int maxR(ArrayList<Integer> reScore) {
-      if(reScore.size() == 0)
-        return 0;
-      Collections.sort(reScore);
-      return reScore.get(reScore.size() - 1);
-    }
-
-    boolean vMap2[]; // visit Map
-    void resetVMp2() { vMap2 = new boolean[NUM_NODES()]; }
-    boolean visitedVMp2(int v) { return vMap2[v]; }
-    void setVisitedVMp2(int v) { vMap2[v] = true; }
-
-    int R (int v ) { // v == vertex
-      int cr = cachedR(v);
-      if( cr != -1) {
-        return cr;
+    void checkAndsetMaxVals(int r, int v) {
+      if(r > maxR) {
+        maxR = r;
+        V = v;
       }
-
-      if(visitedVMp2(v)) {
-        return -500000000;
-      }
-      setVisitedVMp2(v);
-
-      ArrayList<Integer> edges = paths.get(v);
-      if(edges == null) {
-        setCachedR(v, 0);
-        return 0;
-      }
-
-      ArrayList<Integer> re_ai = new ArrayList<Integer>(edges.size());
-
-      for(int i=0; i<edges.size(); i++) {
-        int v1 = edges.get(i) , r = -1;
-        if(visitedVMp2(v1)) {
-          resetVisitMap();
-          dfs(v1);
-          r = rcount;
-          setCachedR(v1, r);
-        } else {
-          r = R(v1);
-        }
-
-        int cr2 = cachedR(v);
-        if( cr2 != -1) {
-          return cr2;
-        }
-
-        r += 1;
-        re_ai.add(r);
-      }
-      int mr = maxR(re_ai);
-      setCachedR(v, mr);
-      return mr;
-    }
-
-    int getResult() {
-      int prc = -1 , pv = -1;
-      System.out.println(" getResult ");
-      for(int i=0; i< chdR.length; i++) {
-        int r = chdR[i];
-        if(r == -1)
-          continue;
-        System.out.println(" v "+i+" r "+r);
-        if(r > prc) {
-          prc = r;
-          pv = i;
-        }
-      }
-      return pv;
     }
 
     void solve(int caseid) {
-      initCachedR();
-      for(int i=0; i < NUM_NODES() - 5; i++) {
-        if (paths.get(i) != null) {
-          resetVMp2();
-          R(i);
+      for(int i=0; i<paths.size(); i++) {
+        int e = getConnection(i);
+        if(e == -1)
+          continue;
+        // System.out.println("****** solve i "+i);
+        int r = getMaxReachable(i);
+        if(r == -1) {
+          initForDfs();
+          dfs(i);
+          r = getMaxReachable(i);
         }
+        checkAndsetMaxVals(r, i);
       }
-      System.out.println("Case "+caseid+": "+getResult());
+      System.out.println("Case "+caseid+": "+V);
     }
   }
 
   public static void main(String s[]) {
     Scanner sc = new Scanner(System.in);
     int caseid = 1;
+    FE fe = new Main(). new FE();
     while(sc.hasNext()) {
       int T = Integer.parseInt(sc.next());
       for(int t = 0; t < T; t++) {
-        int N = Integer.parseInt(sc.next());
-        FE fe = new Main(). new FE();
+        int N = Integer.parseInt(sc.next());      
+        fe.clearAll();
         for(int n = 0; n < N; n++) {
           int u = Integer.parseInt(sc.next());
           int v = Integer.parseInt(sc.next());
