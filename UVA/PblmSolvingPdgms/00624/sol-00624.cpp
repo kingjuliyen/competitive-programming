f80b746c4cfdec5bf23e05851779ba78dd02c432
SenthilKumar.Thangavelu <kingjuliyen@gmail.com>
2017-08-14 14:11:14 +0530

    sk2

--- UVA/PblmSolvingPdgms/00624/sol-00624.cpp
+++ UVA/PblmSolvingPdgms/00624/sol-00624.cpp
@@ -2,36 +2,82 @@
 // https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=8&page=show_problem&problem=565
 // author: Senthil Kumar Thangavelu, email: kingjuliyen @ google's email.com
 
-#if 0
-http://cse.unl.edu/~goddard/Courses/CSCE310J/Lectures/Lecture8-DynamicProgramming.pdf
 
-for w = 0 to W
-  B[0,w] = 0
-for i = 1 to n
-  B[i,0] = 0
-
-for i = 1 to n
-    for w = 0 to W
-        if (wi <= w && (bi + B[i-1, w-wi] > B[i-1,w]))
-            B[i, w] = bi + B[i-1, w-wi]
-            P[i, w] = -1 * (w-wi)
-        else
-            B[i, w] = B[i-1, w]
-            P[i, w] = w
-
-#endif
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
+template <typename T>
+struct M2D  {
+  int R, C;
+  vector<T>  m;
+  int SZ() { return R*C; }
+  M2D(int _R, int _C, T initval) : R(_R), C(_C) {  m = vector<T> (SZ(), initval); }
+  M2D(int _R, int _C) : R(_R), C(_C) {  m = vector<T> (SZ()); }
+  ~M2D() { }
+  void set(int r, int c, T val) { m[r*C + c] = val; }
+  void set(int x, T val) { m[x] = val; }
+  T get(int r, int c) { return m[r*C + c]; }
+  void print() { for(int r=0; r<R; r++) { for (int c=0; c<C; c++)    printf("%10d ", get(r, c));     printf("\n");   }  }
+  void test() { for(int i=0; i<SZ(); i++) { set(i, i+1);}  }
+};
+
 class CD {
 public:
-  int N, nt;
+  const int N, nt;
   vector<int> dtn; // duration
-  CD(int _N, int _nt): N(_N), nt(_nt) { }
+  M2D<int> *B, *P;
+  CD(int _N, int _nt): N(_N), nt(_nt), B(new M2D<int>(nt+1, N+1, 0)), P(new M2D<int>(nt+1, N+1, 0)) { }
+  ~CD() { delete B; delete P; }
   void addDuration(int m) { dtn.push_back(m); }
-};
+  int weight(int x) { return dtn[x]; }
+  int benefit(int x) { return dtn[x]; }
+
+#if 0
+http://cse.unl.edu/~goddard/Courses/CSCE310J/Lectures/Lecture8-DynamicProgramming.pdf
+
+for w = 0 to W
+    B[0,w] = 0
+for i = 1 to n
+    B[i,0] = 0
+
+for i = 1 to n
+    for w = 0 to W
+        if wi <= w // item i can be part of the solution
+            if bi + B[i-1,w-wi] > B[i-1,w]
+                B[i,w] = bi + B[i-1,w-wi]
+            else
+                B[i,w] = B[i-1,w]
+        else B[i,w] = B[i-1,w] // wi > w
+#endif
+
+#define wi (weight(i))
+#define bi (benefit(i))
+#define BG(x, y) (B->get(x, y))
+
+  void solve() {
+    const int W = N, I = nt;
+
+    for(int i=1; i <= I; i++) {
+      for(int w=0; w <= W; w++) {
+        if(wi <= w) {
+          if((bi + BG(i-1, w-wi)) > BG(i-1, w)) {
+            B->set(i, w, (bi + BG(i-1, w-wi)));
+          }
+          else {
+            B->set(i, w, BG(i-1, w));
+          }
+        }
+        else {
+          B->set(i, w, BG(i-1, w));
+        }
+      }
+    }
+    // B->print();
+  } // void solve()
+
+}; // class CD {
 
 int main() {
   int N, nt, m;
@@ -42,5 +88,6 @@ int main() {
       cin >> m;
       cd.addDuration(m);
     }
+    cd.solve();
   }
 }
