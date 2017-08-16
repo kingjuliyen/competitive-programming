cc53fd6aaa276ca2c62b2b13bd936e62262621ef
Senthil Kumar Thangavelu <kingjuliyen@gmail.com>
2017-08-16 14:01:12 +0530

    sk3

--- UVA/PblmSolvingPdgms/10567/sol-10567.cpp
+++ UVA/PblmSolvingPdgms/10567/sol-10567.cpp
@@ -4,15 +4,48 @@
 
 #include <iostream>
 #include <vector>
+#include <set>
 
 using namespace std;
-
+typedef vector< set<int> > vsi_t;
+typedef  set<int> si_t;
 class HFB { // Helping Fill Bates
 public:
-  char * S;
-  HFB(char * _S): S(_S) { }
-  void solve(char *ss) {
+  char * S; size_t Slen;
+  vsi_t charIndexMp;
+  HFB(char * _S): S(_S), Slen(strlen(S)), charIndexMp(vsi_t ('z'+5) )  { recordCharPositions(); }
+  
+  void addIndex(char c, int p) { si_t &s = charIndexMp[c]; s.insert(p); }
+
+  void recordCharPositions() {
+    int lc = Slen/4; // loopcount
+    for(int i=0; i<lc; i++) {
+      int d = i*4;
+      addIndex(S[d+0], d+0); addIndex(S[d+1], d+1); addIndex(S[d+2], d+2); addIndex(S[d+3], d+3); 
+    }
+
+    for(int i=lc*4; i<Slen; i++)
+      addIndex(S[i], i);
   }
+
+  int find(char c, int minStartPos ) {
+    si_t &s = charIndexMp[c];
+    auto it = s.lower_bound(minStartPos);
+    return (it == s.end()) ? -1 : std::distance(s.begin(), it);
+  }
+  
+  pair<int, int> solve(char *s) {
+    int l = strlen(s);
+    sp = -1, cp = 0;
+    for(int i=0; i<l; i++) {
+      cp = find(s[i], cp);
+      if(i == 0)
+        sp = cp;
+      if(cp == -1 || i == l-1)
+        break;
+      cp++;
+    }
+  } // solve(char *s)
 };
 
 int main() {
