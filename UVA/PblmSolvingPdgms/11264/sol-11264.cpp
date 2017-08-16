ffbe0d6afd36b73ba9a3e7db308cd5ae67b3b5a6
Senthil Kumar Thangavelu <kingjuliyen@gmail.com>
2017-08-16 17:44:44 +0530

    boiler plated

---
+++ UVA/PblmSolvingPdgms/11264/sol-11264.cpp
@@ -0,0 +1,29 @@
+// 11264 - Coin Collector
+// https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=2231
+// author: Senthil Kumar Thangavelu, email: kingjuliyen @ google's email.com
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class CC { // Coin Collector
+  public:
+  int n;
+  vector<int> ct; // coin types
+  CC(int _n) :n(_n) { }
+  void addCoin(int c) { ct.push_back(c); }
+  void solve() { }
+};
+
+int main() {
+  int T, n, c;
+  cin >> T;
+  while(T--) {
+    cin >> n;
+    CC cc(n);
+    while(n--) {
+      cin >> c;
+      cc.addCoin(c);
+    }
+    cc.solve();
+  }
+}
