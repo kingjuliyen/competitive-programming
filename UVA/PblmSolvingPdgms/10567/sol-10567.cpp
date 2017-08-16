c1d0569de2d0a033bceb990da2d9db12c56999e5
Senthil Kumar Thangavelu <kingjuliyen@gmail.com>
2017-08-16 16:29:10 +0530

    cln up

--- UVA/PblmSolvingPdgms/10567/sol-10567.cpp
+++ UVA/PblmSolvingPdgms/10567/sol-10567.cpp
@@ -8,23 +8,19 @@
 #include <set>
 
 using namespace std;
-typedef vector< set<int> > vsi_t;
-typedef  set<int> si_t;
+typedef vector< set<int> > vsi_t; typedef  set<int> si_t;
+
 class HFB { // Helping Fill Bates
 public:
-  char * S; size_t Slen;
-  vsi_t charIndexMp;
-  HFB(char * _S): S(_S), Slen(strlen(S)), charIndexMp(vsi_t ('z'+5) )  { recordCharPositions(); }
-  
+  char * S; size_t Slen; vsi_t charIndexMp;  int sp, ep;
+  HFB(char * _S): S(_S), Slen(strlen(S)), charIndexMp(vsi_t ('z'+5) )  { recordCharPositions(); } 
   void addIndex(char c, int p) { si_t &s = charIndexMp[c]; s.insert(p); }
-
   void recordCharPositions() {
     int lc = Slen/4; // loopcount
     for(int i=0; i<lc; i++) {
       int d = i*4;
       addIndex(S[d+0], d+0); addIndex(S[d+1], d+1); addIndex(S[d+2], d+2); addIndex(S[d+3], d+3); 
     }
-
     for(int i=lc*4; i<Slen; i++)
       addIndex(S[i], i);
   }
@@ -34,28 +30,21 @@ public:
     auto it = s.lower_bound(msp);
     return (it == s.end()) ? -1 : *it;
   }
-  
-  int sp, ep;
+
   void solve(char *s) {
     int l = strlen(s), cp = 0;
     sp = -1, cp = 0, ep = -1;
-    for(int i=0; i<l; i++) {
-      // cout << " s[i] " << s[i] << " cp " << cp << endl;
+    for(int i=0; i<l; i++, cp++) { // note cp++
       if((cp = find(s[i], cp)) == -1)
         break;
-      // cout << " new cp " << cp << endl;
-
       if(i == 0)
         sp = cp;
       if(i == l-1)
         ep = cp;
-
-      cp++;
     }
   } // solve(char *s)
 
   void printResult() {
-    // cout << " ### sp " << sp << " ep " << ep << endl;
     if(sp>=0 && ep != -1)
       cout << "Matched " << sp << " " << ep << endl;
     else
@@ -64,20 +53,14 @@ public:
 };
 
 int main() {
-  const int  sLen = 1000000 + 5, ssLen = 101 + 5;
+  int  sLen = 1000000 + 5, ssLen = 101 + 5, Q = -1;
   char *S = new char[sLen], *SS = new char[sLen];
-
-  scanf("%s", S); // cout << S << endl;
+  scanf("%s", S);
   HFB hfb(S);
-
-  int Q = -1;
   scanf("%d", &Q); // cout << Q << endl;
-
   while(Q--) {
     scanf("%s", SS); // cout << SS << endl;
     hfb.solve(SS);
     hfb.printResult();
-    // cout << " *************************************** " << endl;
-
   }
 }
