1eb810740958293184b384c122ebd40eb0be19fd
Senthil Kumar Thangavelu <kingjuliyen@gmail.com>
2017-08-23 15:25:59 +0530

    Solved problem

--- UVA/PblmSolvingPdgms/00787/Main.java
+++ UVA/PblmSolvingPdgms/00787/Main.java
@@ -2,30 +2,21 @@
 // https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=9&page=show_problem&problem=728
 // author: Senthil Kumar Thangavelu, email: kingjuliyen @ google's email.com
 
-import java.util.*;
-import java.lang.*;
-import java.math.*;
+import java.util.*; import java.lang.*; import java.math.*;
+/*
+  Refer to howto.png, there is some dp pattern, note single number by itself is a sequence
+*/
 
 public class Main {
     class MSP { // Maximum Subsequence Product
-      Vector <BigInteger> vi; int R, C; BigInteger max; BigInteger[][] mtx; // matrix
-      MSP() { vi = new Vector <BigInteger>(); max = new BigInteger(Integer.toString(-999999)); }
+      Vector <BigInteger> vi; int R, C; BigInteger high; BigInteger[][] mtx; // matrix
+      MSP() { vi = new Vector <BigInteger>(); high = new BigInteger(Integer.toString(-999999)); }
       void add(BigInteger x) { vi.add(x); }
-      
-      void setVal(int r, int c, BigInteger bi) {
-        mtx[r][c] = bi;
-        max = max.max(bi);
-      }
+      void setVal(int r, int c, BigInteger bi) { mtx[r][c] = bi; high = high.max(bi); }
       
       void solve() {
-        if(vi.size() ==1) {
-          System.out.println(vi.get(0).toString());
-          return;
-        }
-        
-        R = C = vi.size(); R--;
-        mtx = new BigInteger[R][C];
-        for(int r= R-1; r>=0; r--) {
+        R = C = vi.size(); mtx = new BigInteger[R][R];
+        for(int r= R-2; r>=0; r--)
           for(int c = C-1; c>=0; c--) {
             if(c == r+1) {
               setVal(r,c, vi.get(r).multiply(vi.get(c)));
@@ -33,26 +24,22 @@ public class Main {
             }
             setVal(r,c, vi.get(r).multiply(mtx[r+1][c]));
           }
-        }
-        System.out.println(max.toString());
+        for(int c=0; c<R; c++) { setVal(R-1, c, vi.get(c)); } // single number by itself is a sequence
+        System.out.println(high.toString());
       } // void solve()
+    } // class MSP
 
-    } // class MSP 
-  
     public static void main(String args[]) {
       Scanner sc = new Scanner(System.in);
-      
       while(sc.hasNext()) {
         MSP msp = new Main(). new MSP();
         while(true) {
           int i = Integer.parseInt(sc.next());
-          if(i == -999999) {
+          if(i == -999999)
             break;
-          }
           msp.add(new BigInteger(Integer.toString(i)));
         }
         msp.solve();
       }
-
-    }
+    } // public static void main(String args[])
 }
