fce5c28bdf5a9a6954838662b30c252edc085420
Senthil Kumar Thangavelu <kingjuliyen@gmail.com>
2017-08-30 14:27:10 +0530

    baseimpl

--- UVA/PblmSolvingPdgms/00481/sol-00481.cpp
+++ UVA/PblmSolvingPdgms/00481/sol-00481.cpp
@@ -7,177 +7,81 @@
 #include <set>
 #include <list>
 #include <limits>
+#include <cmath>
 
 using namespace std;
 typedef vector<int> vi_t;
-typedef vector<int> * vi_tp;
 
-struct TMX {
-  vi_tp v;
-  int tmx; // tail max value at tail at
-  TMX(vi_tp _v, int t) : v(_v), tmx(t) { }
-  bool operator<(const TMX &rhs) const { return tmx < rhs.tmx; }
-  bool operator>(const TMX &rhs) const { return tmx > rhs.tmx; }
-  bool operator==(int x) const { return tmx == x; }
-  // bool operator=(const TMX &rhs) { v = rhs.v; tmx = rhs.tmx; }
-};
+struct WGU {
+  vi_t X;
+  int N, L,lo, hi, newL, mid,k        ,*P, *M, *S;
+  WGU() { }
+  void add(int x) { X.push_back(x); }
+#if 0
+P[N]; M[N+1];  L = 0;
 
-struct MXL {
-  vi_tp v;
-  int ml; // maxlen
-};
+for i in range 0 to N-1: // Binary search for the largest positive j ≤ Lsuch that X[M[j]] < X[i]
+  lo = 1; hi = L
+  while lo ≤ hi:
+    mid = ceil((lo+hi)/2)
+    if X[M[mid]] < X[i]:
+      lo = mid+1
+    else:
+      hi = mid-1
 
-class WGU {
-  public:
-  vi_t vi;
-  list< vi_tp > lst;
-  set<TMX> stmx;
-  TMX sm, lg;
+  newL = lo // After searching, lo is 1 greater than the length of the longest prefix of X[i]
+  P[i] = M[newL-1] // The predecessor of X[i] is the last index of the subsequence of length newL-1
+  M[newL] = i
 
-  WGU(): sm(0, numeric_limits<int>::max()), lg(0, numeric_limits<int>::min()) { }
+  if newL > L: // If we found a subsequence longer than any we've found yet, update L
+    L = newL
+// ============================================
+// Reconstruct the longest increasing subsequence
+S[L]
+k = M[L]
+for i in range L-1 to 0:
+  S[i] = X[k]
+  k = P[k]
 
-  void print() {
-    for(auto &t : stmx) {
-      //cout << " ti.v " << t.v << " t.ml " << t.tmx << endl;
-      auto &v = *t.v;
-      //cout << " vi.size () " << v.size() << endl;
+#endif
 
-      for(int i=0; i<v.size(); i++) {
-        cout << " " << v[i];
+
+  void algo() {
+    for(int i=0; i<N; i++) {
+      lo = 1; hi = L;
+      while(lo <= hi) {
+        mid = ceil((hi+lo)/2);
+        if(X[M[mid]] < X[i])
+          lo = mid+1;
+        else
+          hi = mid-1;
       }
-      cout << endl << "--" << endl;
+      newL = lo;
+      P[i] = M[newL - 1];
+      M[newL] = i;
+      if(newL > L)
+        L = newL;
     }
   }
 
-  void checkNsetIfSmallest(TMX &t) {
-    if(t<sm)
-      sm = t;
-  }
-
-  void checkNsetIfLargest(TMX &t) {
-    if(t>lg)
-      lg = t;
-  }
-
-  void add(int x) { vi.push_back(x); }
-  bool smallest(int c) { return c < sm.tmx; }
-  bool largest(int c) { return c > lg.tmx; }
-
-  void newActiveList(int c) {
-    vi_tp vp = new vi_t ();
-    vp->push_back(c);
-    TMX t(vp, vp->at(0));
-    stmx.insert(t);
-    checkNsetIfSmallest(t);
-    checkNsetIfLargest(t);
-  }
-
-  TMX longest() {
-    int m = 0; TMX t(0,0);
-
-    for(auto s: stmx) {
-      vi_tp v = s.v;
-      if(v->size() > m) {
-        m = v->size();
-        t = s;
-      }
+  void reconstruct() {
+    S = new int[L];
+    k = M[L];
+    for(int i=L-1; i>=0; i--) {
+      S[i] = X[k];
+      cout << " " << S[i];
+      k = P[k];
     }
-    return t;
-  }
-
-  void handleLargest(int c) {
-    TMX t = longest();
-    vi_tp v = t.v;
-    vi_tp vp = new vi_t (*t.v);
-    vp->push_back(c);
-    // cout << " handleLargest vp->size() " << vp->size();
-    t = TMX(vp, vp->at(vp->size()-1));
-    stmx.insert(t);
-    checkNsetIfLargest(t);
-  }
-
-  void handleInBetween(int c) {
-    // cout << " handleInBetween " << endl;
-    int m = numeric_limits<int>::max();
-    vi_tp n = 0;
-
-    for(auto s: stmx) {
-      vi_tp v = s.v;
-      int d = c - (v->at(v->size()-1));
-      if(!d)
-        return;
-
-      if(d>0 && d<m ) {
-        m = d;
-        n = v;
-      }
-    }
-    // cout << " handleInBetween 3 n " << n << endl;
-    vi_tp vp = new vi_t (*n);
-    vp->push_back(c);
-    int rsz = vp->size();
-    // cout << " handleInBetween 4 " << endl;
-    auto it = stmx.begin();
-    while(it != stmx.end()) {
-      vi_tp v = it->v;
-      if(v->size() == rsz) {
-        // it->v->clear();
-        // delete(it->v);
-        it = stmx.erase(it);
-      } else {
-        it++;
-      }
-    }
-    // cout << " handleInBetween 4 " << endl;
-    auto t = TMX(vp, vp->at(vp->size()-1));
-    stmx.insert(t);
-    checkNsetIfLargest(t);
-    checkNsetIfSmallest(t);
   }
 
   void solve() {
-    newActiveList(vi[0]);
-
-    for(int i=1; i< vi.size(); i++) {
-
-      int c = vi[i];
-      // cout << "========== " << c << endl;
-      if(smallest(c)) {
-        // cout << " smallest(c) " << endl;
-        newActiveList(c);
-      }
-      else if(largest(c)) {
-        // cout << " largest(c) " << endl;
-        handleLargest(c);
-      }
-      else {
-        handleInBetween(c);
-      }
-      // print();
-    }
-    // cout << endl;
-
+    N = X.size(); P = new int[N]; M = new int[N+1]; L = 0;
+    algo();
+    reconstruct();
   } // void solve()
 
   void printResult() {
-    int m = 0;
-    vi_tp n = 0;
-
-    for(auto s: stmx) {
-      vi_tp v = s.v;
-      if(v->size() > m) {
-        m = v->size();
-        n = v;
-      }
-    }
-
-    cout << n->size() << endl;
-    cout << "-";
-    for(int i=0; i<n->size(); i++) {
-      cout << endl << n->at(i);
-    }
-    cout << endl;
-  }
+  } // printResult()
 };
 
 int main() {
